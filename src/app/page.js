05669654d8dd477358d266e2688dbf5da9886670
Saif Ullah Sajid <saifbinsajid7@gmail.com>
2023-11-08 21:05:22 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -57,22 +57,10 @@ export default function Home() {
             <a class="mr-5 text-white hover:text-gray-300">Our Products</a>
             <a class="mr-5 text-white hover:text-gray-300">Services</a>
             <a class="mr-5 text-white hover:text-gray-300">Our Clients</a>
-            <a class="mr-5 text-white hover:text-gray-300">Contact Us</a>
           </nav>
-          {/* <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-4 h-4 ml-1"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button> */}
+          <a class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Contact Us
+          </a>
         </div>
       </header>
       <section class="text-gray-400 body-font">
